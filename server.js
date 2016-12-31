50d30b5df7a0ffc708b5fad48a7676b1b1b7417d
Yasser Hussain <yasserhussain1110@gmail.com>
2016-12-31 18:20:02 +0530

    App built

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var multer = require('multer');
+var upload = multer();
+
 var app = express();
 
-app.get('/api/latest/imagesearch', function (req, res) {
-  sendLatestSearchItems(res);
+app.use(express.static('public'));
+
+app.post('/get-file-size', upload.single('document'), function (req, res, next) {
+  if (req.file) {
+    res.json({
+      size: req.file.size
+    });
+  } else {
+    res.status(400).end("File field cannot be empty.");
+  }
 });
-
-
 
 app.listen(process.env.PORT || 8080);
 
